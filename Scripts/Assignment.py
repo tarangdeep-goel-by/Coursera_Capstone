4c3beb60c31aa304f867483413706d8423bbcd4f
tarangdg <tarangdeepgoel2000@gmail.com>
2020-10-03 15:30:50 +0530

    Week 1 Peer Graded Assignment

---
+++ Scripts/Assignment.py
@@ -0,0 +1,6 @@
+# This is the Final Capstone Project for the IBM Data Science Professionl
+# Certificate by Tarang Deep Goel
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course")
